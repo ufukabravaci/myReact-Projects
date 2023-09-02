4224b3550b01e49a45151ae3d2e2a0e093526a34
ufukabravaci <ufkk1262@gmail.com>
2023-09-02 16:09:49 +0300

    fetching data from OMDB API

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App.js";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
     />
-    <StarRating size={24} color="red" maxRating={5} className="test" />
+    <StarRating size={24} color="red" maxRating={5} className="test" /> */}
   </React.StrictMode>
 );
